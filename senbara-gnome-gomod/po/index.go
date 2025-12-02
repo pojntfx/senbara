97e7c36e02f0d5628e58f53d1a97273064fc483c
Felicitas Pojtinger <felicitas@pojtinger.com>
2025-12-01 23:42:45 -0800

    refactor: Port to new flattened i18n structure
    Signed-off-by: Felicitas Pojtinger <felicitas@pojtinger.com>

--- senbara-gnome-gomod/po/index.go
+++ senbara-gnome-gomod/po/index.go
@@ -3,8 +3,8 @@ package po
 import "embed"
 
 //go:generate sh -c "find ../src -name '*.go' -o -name '*.blp' | xgettext --language=C++ --keyword=_ --keyword=L --omit-header -o senbara-gnome-gomod.pot --files-from=-"
-//go:generate sh -c "find . -name 'senbara-gnome-gomod.po' -print0 | xargs -0 -I {} msgmerge --update --backup=none \"{}\" senbara-gnome-gomod.pot"
-//go:generate sh -c "find . -type f -name '*.po' -print0 | xargs -0 -I {} sh -c 'msgfmt -o \"{}.mo\" \"{}\"' && find . -type f -name '*.po.mo' -exec sh -c 'mv \"{}\" \"$(echo \"{}\" | sed s/\\.po\\.mo/.mo/)\"' \\;"
-//go:generate sh -c "ls -d */ | sed s@/@@g > LINGUAS"
+//go:generate sh -c "find . -name '*.po' -print0 | xargs -0 -I {} msgmerge --update --backup=none \"{}\" senbara-gnome-gomod.pot"
+//go:generate sh -c "find . -type f -name '*.po' -print0 | xargs -0 -I {} sh -c 'mkdir -p $(basename {} .po)/LC_MESSAGES && msgfmt -o $(basename {} .po)/LC_MESSAGES/senbara-gnome-gomod.mo {}'"
+//go:generate sh -c "find . -name \"*.po\" -exec basename {} .po \\; > LINGUAS"
 //go:embed *
 var FS embed.FS
