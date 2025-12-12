4bf125b8def0a1927fc2a9d7913c36a6f936a18a
Felicitas Pojtinger <felicitas@pojtinger.com>
2025-12-11 23:46:12 -0800

    refactor: Drop manual WebKit initialization

--- senbara-gnome/cmd/senbara-gnome/main.go
+++ senbara-gnome/cmd/senbara-gnome/main.go
@@ -49,8 +49,6 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	_ = webkit.WebViewGLibType() // TODO: WebKit should initialize itself automatically
-
 	level := new(slog.LevelVar)
 	log := slog.New(slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
 		Level: level,
